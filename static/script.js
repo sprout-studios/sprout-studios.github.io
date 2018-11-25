14b5cba8433fc3b810d3f6be2aeaeb3569a32607
Santosh Kumar <sntshkmr60@gmail.com>
2018-11-25 22:35:52 +0530

    adds scroll to top functionality

--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // turn the carousel alive
   $('.carousel').carousel({
     interval: 10000
   });
@@ -9,6 +10,20 @@ $(document).ready(function () {
 
   $('#team-carousel').carousel({
     interval: 20000
+  });
+
+  // scroll to top button is hidden when inside fold
+  window.onscroll = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("scroll-to-top").style.display = "block";
+    } else {
+      document.getElementById("scroll-to-top").style.display = "none";
+    }
+  }
+
+  $('#scroll-to-top').click(() => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   });
 
 });
